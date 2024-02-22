dc7d460fc9bb02460d421574679a70cc621b8db9
glaboy02 <glaboy02@gmail.com>
2024-02-21 22:37:57 -0500

    Adding a new SPP file for monthly and yearly

---
+++ URV/Scraping/filteredDateYearly_SPP.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+
+# df = pd.read_csv('spp_test7.csv', index_col='datetime').rename_axis(None)
+
+# print(df.head())
+
+# Scraping\spp_test7.csv
+
+df = pd.read_csv('URV/Scraping/spp_test7.csv')
+
+print(df.head())
+
+df['datetime'] = pd.to_datetime(df['datetime'])
+
+grouped_df = df.groupby(df['datetime'].dt.year)['totalAVGIntensity']
+
+print(grouped_df.groups.keys())
+
+mean_df = grouped_df.mean()
+
+
+std_df = grouped_df.std()
+
+print(mean_df)
+
+mean_df.plot.bar(yerr= std_df, capsize = 4)
+plt.xticks(rotation = 360)
+plt.tick_params(left = False, bottom = False)
+plt.ylabel(r'Average Carbon Intensity (g.CO$_2$eq/kWh)',fontsize = 10)
+plt.xlabel(r'Year',fontsize = 10)
+plt.ylim([0,600])
+
+# mean_df.plot.pie(yerr = std_df,autopct='%1.1f%%')
+
+
+savename = 'URV/Scraping/Plots/Yearly_SPP'
+
+plt.savefig(savename)
+
+plt.close()
+
